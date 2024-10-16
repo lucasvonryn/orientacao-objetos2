484d18b223bf6235e31eaf600b9ae2834913861c
Lucas Von Ryn <lvonryn@gmail.com>
2024-10-15 21:44:43 -0300

    Exercícios da aula 05

---
+++ OrientacaoObjeto2/src/aula05/ex01/CalculadoraTeste.java
@@ -0,0 +1,11 @@
+package aula05.ex01;
+
+public class CalculadoraTeste {
+
+    public static void main(String[] args) {
+        
+        Calculadora calc = new Calculadora();
+
+        calc.calcularRaizQuadrada();
+    }
+}
