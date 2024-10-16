c62ff8b36d9be5c8ef14d99115170bf6ef33ffdf
Lucas Von Ryn <lvonryn@gmail.com>
2024-10-16 07:15:06 -0300

    Iniciei o ex02 da aula 05

---
+++ OrientacaoObjeto2/src/aula05/ex02/Pessoa.java
@@ -0,0 +1,16 @@
+package aula05.ex02;
+
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private int cpf;
+    private String logradouro;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private int cep;
+
+    
+}
---
+++ OrientacaoObjeto2/src/aula05/ex02/PessoaTeste.java
@@ -0,0 +1,8 @@
+package aula05.ex02;
+
+public class PessoaTeste {
+
+    public static void main(String[] args) {
+        
+    }
+}
