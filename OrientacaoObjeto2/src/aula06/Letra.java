fc396a5835b45910b478a0e57a7dcf982a69c2f0
Lucas Von Ryn <lvonryn@gmail.com>
2024-10-28 21:44:58 -0300

    Iniciei o ex01 da aula06

---
+++ OrientacaoObjeto2/src/aula06/Letra.java
@@ -0,0 +1,33 @@
+package aula06;
+
+public class Letra {
+
+    private String string1;
+    private String string2;
+
+    public void solicitarStrings(String string1, String string2) {
+
+        this.string1 = string1;
+        this.string2 = string2;
+    }
+
+    public void compararStrings(String string1, String string2) {
+
+        
+    }
+
+    public void contarCacteresString() {
+
+
+    }
+
+    public void converterMinusculo(String string) {
+
+        
+    }
+
+    public void converterMaiusculo(String string) {
+
+
+    }
+}
---
+++ OrientacaoObjeto2/src/aula06/LetraTeste.java
@@ -0,0 +1,13 @@
+package aula06;
+
+public class LetraTeste {
+
+    public static void main(String[] args) {
+        
+        Letra letra = new Letra();
+
+        letra.solicitarStrings("Teste", "Lucas Von Ryn");
+        
+        
+    }
+}
