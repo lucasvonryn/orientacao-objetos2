41231f7cdc033267e3a027a8294350beb88a42b9
Lucas Von Ryn <lvonryn@gmail.com>
2024-10-31 16:54:49 -0300

    Fiz o ex01 da aula06

--- OrientacaoObjeto2/src/aula06/ex01/LetraTeste.java
+++ OrientacaoObjeto2/src/aula06/ex01/LetraTeste.java
@@ -1,13 +1,40 @@
 package aula06.ex01;
 
+import java.util.InputMismatchException;
+
 public class LetraTeste {
 
     public static void main(String[] args) {
-        
-        Letra letra = new Letra();
 
-        letra.solicitarStrings("Teste", "Lucas Von Ryn");
+        Letra letra = new Letra();
         
-        
+        // try {
+
+        //     letra.solicitarStrings();
+
+        // } catch (InputMismatchException ime) {
+
+        //     System.out.println("Entrada de dados inválida.");
+
+        // } catch (Exception e) {
+
+        //     System.out.println("Ocorreu uma exceção.");
+        // }
+
+        // try {
+
+        //     letra.compararStrings();
+
+        // } catch (InputMismatchException ime) {
+
+        //     System.out.println("Entrada de dados inválida.");
+
+        // } catch (Exception e) {
+
+        //     System.out.println("Ocorreu uma exceção.");
+        // }
+
+        letra.converterMaiusculo("Teste");
+        letra.converterMinusculo("Teste");
     }
 }
