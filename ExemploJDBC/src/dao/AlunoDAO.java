261946b71209034d8e2c6e580d8180a44dd92663
Lucas Von Ryn <lvonryn@gmail.com>
2025-04-19 17:37:30 -0300

    finalizei o exercícido da aula 11 (crud do aluno)

--- ExemploJDBC/src/dao/AlunoDAO.java
+++ ExemploJDBC/src/dao/AlunoDAO.java
@@ -81,17 +81,87 @@ public class AlunoDAO {
 		}
 	}
 	
-	public Aluno buscarPorRA(int ra) {
+	public Aluno buscarPorRA(int ra) throws SQLException {
 		
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			st = conn.prepareStatement("select * from aluno where registro_academico = ?");
+			
+			st.setInt(1, ra);
+			
+			rs = st.executeQuery();
+			
+			if (rs.next()) {
+				
+				Aluno aluno = new Aluno();
+				
+				aluno.setNome(rs.getString("nome"));
+				aluno.setSexo(rs.getString("sexo"));
+				aluno.setCodigoCurso(rs.getInt("codigo_curso"));
+				aluno.setDataIngresso(rs.getString("data_ingresso"));
+				aluno.setPeriodo(rs.getInt("periodo"));
+				aluno.setCoeficiente(rs.getDouble("coeficiente"));
+				
+				return aluno;
+			}
+			
+			return null;
+			
+		} finally {
+			
+			BancoDados.finalizarStatement(st);
+			BancoDados.finalizarResultSet(rs);
+			BancoDados.desconectar();
+		}
 	}
 	
-	public void atualizar(Aluno aluno) {
+	public void atualizar(Aluno aluno) throws SQLException {
 		
+		PreparedStatement st = null;
+		
+		try {
+			
+			st = conn.prepareStatement("update aluno set nome = ?, sexo = ?, codigo_curso = ?,"
+					+ "data_ingresso = ?, periodo = ?, coeficiente = ? where registro_academico = ?");
+			
+			st.setString(1, aluno.getNome());
+			st.setString(2, aluno.getSexo());
+			st.setInt(3, aluno.getCodigoCurso());
+			st.setString(4, aluno.getDataIngresso());
+			st.setInt(5, aluno.getPeriodo());
+			st.setDouble(6, aluno.getCoeficiente());
+			st.setInt(7, aluno.getRegistroAcademico());
+			
+			st.executeUpdate();
+			
+		} finally {
+			
+			BancoDados.finalizarStatement(st);
+			BancoDados.desconectar();
+		}
 	}
 	
-	public int excluir(int ra) {
+	public int excluir(int ra) throws SQLException {
 		
-		return 0;
+		PreparedStatement st = null;
+		
+		try {
+			
+			st = conn.prepareStatement("delete from aluno where registro_academico = ?");
+			
+			st.setInt(1, ra);
+			
+			int linhasManipuladas = st.executeUpdate();
+			
+			return linhasManipuladas;
+			
+		} finally {
+			
+			BancoDados.finalizarStatement(st);
+			BancoDados.desconectar();
+		}
 	}
 }
--- ExemploJDBC/test/dao/AlunoDAOTeste.java
+++ ExemploJDBC/test/dao/AlunoDAOTeste.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import entities.Aluno;
+import entities.Curso;
 
 public class AlunoDAOTeste {
 
@@ -38,12 +39,68 @@ public class AlunoDAOTeste {
 		}
 	}
 	
+	public static void buscarPorRATeste() throws SQLException, IOException {
+		
+		int registro_academico = 4;
+		
+		Connection conn = BancoDados.conectar();
+		Aluno aluno = new AlunoDAO(conn).buscarPorRA(registro_academico);
+		
+		if (aluno != null) {
+			
+			System.out.println(aluno.getNome() + " - " + aluno.getSexo() + " - " + aluno.getCodigoCurso() + 
+					" - " + aluno.getDataIngresso() + " - " + aluno.getPeriodo() + " - " +
+					aluno.getCoeficiente());
+			
+		} else {
+			
+			System.out.println("RA não encontrado.");
+		}
+	}
+	
+	public static void atualizarAlunoTeste() throws SQLException, IOException {
+		
+		Aluno aluno = new Aluno();
+		aluno.setRegistroAcademico(3);
+		aluno.setNome("Lucas Gabriel Von Ryn");
+		aluno.setSexo("Masculino");
+		aluno.setCodigoCurso(1);
+		aluno.setDataIngresso("01/04/2025");
+		aluno.setPeriodo(5);
+		aluno.setCoeficiente(9.89);
+		
+		Connection conn = BancoDados.conectar();
+		new AlunoDAO(conn).atualizar(aluno);
+		
+		System.out.println("Dados do aluno atualizados com sucesso.");
+	}
+	
+	public static void excluirAlunoTeste() throws SQLException, IOException {
+		
+		int ra = 4;
+		
+		Connection conn = BancoDados.conectar();
+		int linhasManipuladas = new AlunoDAO(conn).excluir(ra);
+		
+		if (linhasManipuladas > 0) {
+			
+			System.out.println("Aluno excluído com sucesso.");
+			
+		} else {
+		
+			System.out.println("Nenhum registro foi alterado.");
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		try {
 			
 //			AlunoDAOTeste.cadastrarCursoTeste();
-			AlunoDAOTeste.buscarTodosAlunosTeste();
+//			AlunoDAOTeste.buscarTodosAlunosTeste();
+//			AlunoDAOTeste.buscarPorRATeste();
+//			AlunoDAOTeste.atualizarAlunoTeste();
+			AlunoDAOTeste.excluirAlunoTeste();
 			
 		} catch (SQLException | IOException e) {
 			
